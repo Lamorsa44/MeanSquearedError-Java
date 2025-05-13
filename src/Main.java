18be8431d4d503a375a4d62eea675aeff91276e7
Lamorsa44 <uebito123@gmail.com>
2025-05-13 02:09:18 -0600

    deeeeam

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N;
+        int[] results;
+        int[] predictions;
+
+        System.out.println("Enter N: ");
+        N = sc.nextInt();
+        results = new int[N];
+        predictions = new int[N];
+
+        System.out.println("Enter table of results");
+        IntStream.range(0, N).forEach(i -> {
+            results[i] = sc.nextInt();
+        });
+
+        System.out.println("Enter table of predictions");
+        IntStream.range(0, N).forEach(i -> {
+            predictions[i] = sc.nextInt();
+        });
+
+        double result = IntStream.range(0, N).mapToDouble(x -> Math.pow(results[x] - predictions[x], 2)).sum() / N;
+        System.out.println("Result: " + result);
+    }
 }
